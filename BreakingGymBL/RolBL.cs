87d5b2ae044e7df5ddc247b60c5171a356f842f1
Alejo-1000 <alejandroore503@gmail.com>
2025-05-31 16:15:50 -0600

    Ya termine RolDAL, RolBL y RolEN

---
+++ BreakingGymBL/RolBL.cs
@@ -0,0 +1,32 @@
+﻿using BreakingGymEN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BreakingGymDAL;
+
+namespace BreakingGymBL
+{
+    public class RolBL
+    {
+        public List<RolEN> MostrarRol()
+        {
+            return RolDAL.MostrarRol();
+        }
+        public int GuardarRol(RolEN prolEN)
+        {
+            return RolDAL.AgregarRol(prolEN);
+        }
+        public int EliminarRol(RolEN prolEN)
+        {
+            return RolDAL.EliminarRol(prolEN);
+        }
+        public int ModificarRol(RolEN prolEN)
+        {
+            return RolDAL.ModificarRol(prolEN);
+        }
+
+    }
+}
