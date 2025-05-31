a40fb3306b96fd3312f91f3f8f5d14c578785346
AldanaMajo <majoaldana178@gmail.com>
2025-05-30 21:59:43 -0600

    Termine Usuario EN,BL,DAL

---
+++ BreakingGymBL/UsuarioBL.cs
@@ -0,0 +1,38 @@
+﻿using BreakingGymDAL;
+using BreakingGymEN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingGymBL
+{
+    public class UsuarioBL
+    {
+        public int VerificarUsuarioLogin(UsuarioEN pusuarioEN)
+        {
+            return UsuarioDAL.VerificarUsuarioLogin(pusuarioEN);
+        }
+        public List<UsuarioEN> MostrarUsuario()
+        {
+            return UsuarioDAL.MostrarUsuario();
+        }
+        public static List<UsuarioEN> BuscarUsuario(string nombre)
+        {
+            return UsuarioDAL.BuscarUsuario(nombre);
+        }
+        public int GuardarUsuario(UsuarioEN pusuarioEN)
+        {
+            return UsuarioDAL.AgregarUsuario(pusuarioEN);
+        }
+        public int EliminarUsuario(UsuarioEN pusuarioEN)
+        {
+            return UsuarioDAL.EliminarUsuario(pusuarioEN);
+        }
+        public int ModificarUsuario(UsuarioEN pusuarioEN)
+        {
+            return UsuarioDAL.ModificarUsuario(pusuarioEN);
+        }
+    }
+}
---
+++ BreakingGymDAL/UsuarioDAL.cs
@@ -0,0 +1,145 @@
+﻿using BreakingGymEN;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingGymDAL
+{
+    public class UsuarioDAL
+    {
+
+        public static int VerificarUsuarioLogin(UsuarioEN pusuarioEN)
+        {
+            using (IDbConnection _conn = ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando =
+                new SqlCommand("VerificarUsuarioLogin", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                _comando.Parameters.Add(new SqlParameter("@IdRol", pusuarioEN.IdRol));
+                _comando.Parameters.Add(new SqlParameter("@Cuenta", pusuarioEN.Cuenta));
+                _comando.Parameters.Add(new SqlParameter("@Contrasenia", pusuarioEN.Contrasenia));
+                int userExiste = (int?)_comando.ExecuteScalar() ?? 0;
+                _conn.Close();
+                return userExiste;
+            }
+        }
+        public static List<UsuarioEN> MostrarUsuario()
+        {
+            List<UsuarioEN> _Lista = new List<UsuarioEN>();
+            using (IDbConnection _conn = ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando =
+                new SqlCommand("MostrarUsuario", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                IDataReader _reader = _comando.ExecuteReader();
+                while (_reader.Read())
+                {
+                    _Lista.Add(new UsuarioEN
+                    {
+                        Id = _reader.GetInt32(0),
+                        IdRol = _reader.GetInt32(1),
+                        Nombre = _reader.GetString(2),
+                        Apellido = _reader.GetString(3),
+                        Celular = _reader.GetString(4),
+                        Cuenta = _reader.GetString(5),
+                        Contrasenia = _reader.GetString(6)
+                    });
+                }
+                _conn.Close();
+            }
+            return _Lista;
+        }
+        public static List<UsuarioEN> BuscarUsuario(String Nombre)
+        {
+            List<UsuarioEN> _Lista = new List<UsuarioEN>();
+            using (IDbConnection _conn = ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando =
+                new SqlCommand("BuscarUsuario", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                _comando.Parameters.Add(new SqlParameter("@Nombre", Nombre));
+                IDataReader _reader = _comando.ExecuteReader();
+                while (_reader.Read())
+                {
+                    _Lista.Add(new UsuarioEN
+                    {
+                        Id = _reader.GetInt32(0),
+                        IdRol = _reader.GetInt32(1),
+                        Nombre = _reader.GetString(2),
+                        Apellido = _reader.GetString(3),
+                        Celular = _reader.GetString(4),
+                        Cuenta = _reader.GetString(5),
+                        Contrasenia = _reader.GetString(6)
+
+                    });
+                }
+                _conn.Close();
+            }
+            return _Lista;
+        }
+        public static int AgregarUsuario(UsuarioEN pusuarioEN)
+        {
+            using (IDbConnection _conn = ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando = new SqlCommand("GuardarUsuario", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                _comando.Parameters.Add(new SqlParameter("@IdRol", pusuarioEN.IdRol));
+                _comando.Parameters.Add(new SqlParameter("@Nombre", pusuarioEN.Nombre));
+                _comando.Parameters.Add(new SqlParameter("@Apellido", pusuarioEN.Apellido));
+                _comando.Parameters.Add(new SqlParameter("@Celular", pusuarioEN.Celular));
+                _comando.Parameters.Add(new SqlParameter("@Cuenta", pusuarioEN.Cuenta));
+                _comando.Parameters.Add(new SqlParameter("@Contrasenia", pusuarioEN.Contrasenia));
+                int resultado = _comando.ExecuteNonQuery();
+                _conn.Close();
+                return resultado;
+            }
+
+        }
+        public static int EliminarUsuario(UsuarioEN pusuarioEN)
+        {
+            using (IDbConnection _conn =
+                ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando =
+                new SqlCommand("EliminarUsuario", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                _comando.Parameters.Add(new SqlParameter("@Id", pusuarioEN.Id));
+                int resultado = _comando.ExecuteNonQuery();
+                _conn.Close();
+                return resultado;
+            }
+        }
+        public static int ModificarUsuario(UsuarioEN pusuarioEN)
+        {
+            using (IDbConnection _conn =
+                ComunBD.ObtenerConexion(ComunBD.TipoBD.SqlServer))
+            {
+                _conn.Open();
+                SqlCommand _comando =
+                new SqlCommand("ModificarUsuario", _conn as SqlConnection);
+                _comando.CommandType = CommandType.StoredProcedure;
+                _comando.Parameters.Add(new SqlParameter("@Id", pusuarioEN.Id));
+                _comando.Parameters.Add(new SqlParameter("@IdRol", pusuarioEN.IdRol));
+                _comando.Parameters.Add(new SqlParameter("@Nombre", pusuarioEN.Nombre));
+                _comando.Parameters.Add(new SqlParameter("@Apellido", pusuarioEN.Apellido));
+                _comando.Parameters.Add(new SqlParameter("@Celular", pusuarioEN.Celular));
+                _comando.Parameters.Add(new SqlParameter("@Cuenta", pusuarioEN.Cuenta));
+                _comando.Parameters.Add(new SqlParameter("@Contrasenia", pusuarioEN.Contrasenia));
+                int resultado = _comando.ExecuteNonQuery();
+                _conn.Close();
+                return resultado;
+            }
+
+        }
+    }
+
+}
