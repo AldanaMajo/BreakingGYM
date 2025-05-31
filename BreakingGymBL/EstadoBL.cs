fe3aee055bb787c62099c54b2472159e2beb9941
Alejo-1000 <alejandroore503@gmail.com>
2025-05-30 23:12:35 -0600

    Ya termine EstadoBl y EstadoDAL

---
+++ BreakingGymBL/EstadoBL.cs
@@ -0,0 +1,31 @@
+﻿using BreakingGymEN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BreakingGymDAL;
+
+namespace BreakingGymBL
+{
+    public class EstadoBL
+    {
+        public List<EstadoEN> MostrarEstado()
+        {
+            return EstadoDAL.MostrarEstado();
+        }
+        public int GuardarEstado(EstadoEN pestadoEN)
+        {
+            return EstadoDAL.AgregarEstado(pestadoEN);
+        }
+        public int EliminarEstado(EstadoEN pestadoEN)
+        {
+            return EstadoDAL.EliminarEstado(pestadoEN);
+        }
+        public int ModificarEstado(EstadoEN pestadoEN)
+        {
+            return EstadoDAL.ModificarEstado(pestadoEN);
+        }
+    }
+}
