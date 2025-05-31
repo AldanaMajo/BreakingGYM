3da3e93ae2433de20283213ae1600fd497b2e9eb
Danyel-ramirez <ddanyelrd@hotmail.com>
2025-05-30 22:23:58 -0600

    Termine CredencialEN

---
+++ BreakingGymEN/CredencialEN.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingGymEN
+{
+    public class CredencialEN
+    {
+        public int Id { get; set; }
+        public int IdUsuario { get; set; } //Foranea
+        public string Usuario { get; set; }
+        public string Contrasenia { get; set; }
+    }
+}
