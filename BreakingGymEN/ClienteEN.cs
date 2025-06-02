d6728057cdd6bd3e14bc99c94e6546db2b054c62
Danyel-ramirez <ddanyelrd@hotmail.com>
2025-06-02 17:03:28 -0600

    Modificacion de Atribusto en ClienteEN

--- BreakingGymEN/ClienteEN.cs
+++ BreakingGymEN/ClienteEN.cs
@@ -10,6 +10,8 @@ namespace BreakingGymEN
     {
         public int Id { get; set; }
         public int IdRol { get; set; }       // Clave foránea hacia Rol
+        public int IdTipoDocumento { get; set; } // Clave foránea hacia TipoDocumento
+        public string Documento { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Celular { get; set; }
