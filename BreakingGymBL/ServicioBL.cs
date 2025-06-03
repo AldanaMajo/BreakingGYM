c2eb9fedbd5fe4bfef8f4dfa1b97318c013d32e6
David25-gif <mp25002@esfe.agape.edu.sv>
2025-06-02 23:24:44 -0600

    Termine ServicioBL, ServicioDAL y cree de nuevo la clase ServicioEN.

---
+++ BreakingGymBL/ServicioBL.cs
@@ -0,0 +1,33 @@
+﻿using BreakingGymEN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//-------------------------------------
+using BreakingGymDAL;
+
+
+
+namespace BreakingGymBL
+{
+    public class ServicioBL
+    {
+        public List<ServicioEN> MostrarServicio()
+        {
+            return ServicioDAL.MostrarServicio();
+        }
+        public int GuardarServicio(ServicioEN pservicioEN)
+        {
+            return ServicioDAL.AgregarServicio(pservicioEN);
+        }
+        public int EliminarServicio(ServicioEN pservicioEN)
+        {
+            return ServicioDAL.EliminarServicio(pservicioEN);
+        }
+        public int ModificarServicio(ServicioEN pservicioEN)
+        {
+            return ServicioDAL.ModificarServicio(pservicioEN);
+        }
+    }
+}
