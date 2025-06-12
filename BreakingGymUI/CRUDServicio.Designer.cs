ddd824007fcb816240440d3dd8b83b92665f0c30
Willy-791 <williamrosa791@gmail.com>
2025-06-11 22:01:47 -0600

    CRUDs nuevos agregados, con diseño

---
+++ BreakingGymUI/CRUDServicio.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace BreakingGymUI
+{
+    partial class CRUDServicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CRUDServicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::BreakingGymUI.Properties.Resources.Fondoo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "CRUDServicio";
+            this.Text = "CRUDServicio";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
