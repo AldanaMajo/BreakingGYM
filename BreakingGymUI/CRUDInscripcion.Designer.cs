6e414dc683a58093851255699f2ecc7f11e7cbc2
Willy-791 <williamrosa791@gmail.com>
2025-06-11 22:40:52 -0600

    Modifique el diseño de Inscripcion

--- BreakingGymUI/CRUDInscripcion.Designer.cs
+++ BreakingGymUI/CRUDInscripcion.Designer.cs
@@ -258,7 +258,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::BreakingGymUI.Properties.Resources.Inicio;
+            this.BackgroundImage = global::BreakingGymUI.Properties.Resources.Fondoo;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(930, 522);
             this.Controls.Add(this.label3);
