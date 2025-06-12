84fc235ddac3b37d74f2012ca39205c5626ffedf
AldanaMajo <majoaldana178@gmail.com>
2025-06-11 22:55:00 -0600

    diseño crud membresia

--- BreakingGymUI/CRUDMembresia.Designer.cs
+++ BreakingGymUI/CRUDMembresia.Designer.cs
@@ -28,7 +28,263 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.MaskedTextBox();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtBuscarMembresia = new System.Windows.Forms.TextBox();
+            this.btnRefrescar = new System.Windows.Forms.Button();
+            this.btnMembresiaBuscar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dgMostrarMenbresia = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtDuracion = new System.Windows.Forms.TextBox();
+            this.cbxIdServicio = new System.Windows.Forms.ComboBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgMostrarMenbresia)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(1089, 518);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 16);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "Id";
+            this.label1.Visible = false;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(713, 13);
+            this.txtPrecio.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtPrecio.Mask = "00000";
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 22);
+            this.txtPrecio.TabIndex = 42;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.btnRegresar.Location = new System.Drawing.Point(1501, 13);
+            this.btnRegresar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(137, 87);
+            this.btnRegresar.TabIndex = 41;
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(1055, 47);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(317, 27);
+            this.label7.TabIndex = 40;
+            this.label7.Text = "Buscar (Nombre de membresia)";
+            // 
+            // txtBuscarMembresia
+            // 
+            this.txtBuscarMembresia.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBuscarMembresia.Location = new System.Drawing.Point(996, 106);
+            this.txtBuscarMembresia.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtBuscarMembresia.Name = "txtBuscarMembresia";
+            this.txtBuscarMembresia.Size = new System.Drawing.Size(463, 34);
+            this.txtBuscarMembresia.TabIndex = 39;
+            // 
+            // btnRefrescar
+            // 
+            this.btnRefrescar.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefrescar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRefrescar.Location = new System.Drawing.Point(1309, 154);
+            this.btnRefrescar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRefrescar.Name = "btnRefrescar";
+            this.btnRefrescar.Size = new System.Drawing.Size(125, 74);
+            this.btnRefrescar.TabIndex = 38;
+            this.btnRefrescar.Text = "Refrescar";
+            this.btnRefrescar.UseVisualStyleBackColor = true;
+            // 
+            // btnMembresiaBuscar
+            // 
+            this.btnMembresiaBuscar.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMembresiaBuscar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnMembresiaBuscar.Location = new System.Drawing.Point(1003, 154);
+            this.btnMembresiaBuscar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMembresiaBuscar.Name = "btnMembresiaBuscar";
+            this.btnMembresiaBuscar.Size = new System.Drawing.Size(125, 74);
+            this.btnMembresiaBuscar.TabIndex = 37;
+            this.btnMembresiaBuscar.Text = "Buscar";
+            this.btnMembresiaBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEliminar.Location = new System.Drawing.Point(319, 370);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(125, 74);
+            this.btnEliminar.TabIndex = 36;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnModificar.Location = new System.Drawing.Point(567, 370);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(125, 74);
+            this.btnModificar.TabIndex = 35;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Showcard Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGuardar.Location = new System.Drawing.Point(70, 370);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(125, 74);
+            this.btnGuardar.TabIndex = 34;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // dgMostrarMenbresia
+            // 
+            this.dgMostrarMenbresia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgMostrarMenbresia.Location = new System.Drawing.Point(421, 512);
+            this.dgMostrarMenbresia.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgMostrarMenbresia.Name = "dgMostrarMenbresia";
+            this.dgMostrarMenbresia.RowHeadersWidth = 51;
+            this.dgMostrarMenbresia.RowTemplate.Height = 24;
+            this.dgMostrarMenbresia.Size = new System.Drawing.Size(745, 315);
+            this.dgMostrarMenbresia.TabIndex = 33;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(49, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 27);
+            this.label6.TabIndex = 32;
+            this.label6.Text = "Nombre";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(29, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 27);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Id Servicio";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(602, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 27);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Precio $";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(11, 230);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 27);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Descripcion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(591, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 27);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Duracion";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescripcion.Location = new System.Drawing.Point(158, 220);
+            this.txtDescripcion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(724, 34);
+            this.txtDescripcion.TabIndex = 27;
+            // 
+            // txtDuracion
+            // 
+            this.txtDuracion.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDuracion.Location = new System.Drawing.Point(713, 113);
+            this.txtDuracion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtDuracion.MaxLength = 10;
+            this.txtDuracion.Name = "txtDuracion";
+            this.txtDuracion.Size = new System.Drawing.Size(187, 34);
+            this.txtDuracion.TabIndex = 26;
+            // 
+            // cbxIdServicio
+            // 
+            this.cbxIdServicio.DisplayMember = "Nombre";
+            this.cbxIdServicio.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxIdServicio.FormattingEnabled = true;
+            this.cbxIdServicio.Location = new System.Drawing.Point(158, 119);
+            this.cbxIdServicio.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cbxIdServicio.Name = "cbxIdServicio";
+            this.cbxIdServicio.Size = new System.Drawing.Size(205, 35);
+            this.cbxIdServicio.TabIndex = 25;
+            this.cbxIdServicio.ValueMember = "Id";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(158, 7);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtNombre.MaxLength = 30;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(205, 34);
+            this.txtNombre.TabIndex = 24;
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(1131, 515);
+            this.txtId.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(132, 22);
+            this.txtId.TabIndex = 23;
+            this.txtId.Visible = false;
             // 
             // CRUDMembresia
             // 
@@ -36,13 +292,58 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::BreakingGymUI.Properties.Resources.Fondoo;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(801, 589);
+            this.ClientSize = new System.Drawing.Size(1668, 838);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtBuscarMembresia);
+            this.Controls.Add(this.btnRefrescar);
+            this.Controls.Add(this.btnMembresiaBuscar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.dgMostrarMenbresia);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtDuracion);
+            this.Controls.Add(this.cbxIdServicio);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtId);
             this.Name = "CRUDMembresia";
             this.Text = "CRUDMembresia";
+            ((System.ComponentModel.ISupportInitialize)(this.dgMostrarMenbresia)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox txtPrecio;
+        private System.Windows.Forms.Button btnRegresar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtBuscarMembresia;
+        private System.Windows.Forms.Button btnRefrescar;
+        private System.Windows.Forms.Button btnMembresiaBuscar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dgMostrarMenbresia;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtDuracion;
+        private System.Windows.Forms.ComboBox cbxIdServicio;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtId;
     }
 }
